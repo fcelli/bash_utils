64358a280ee3c6b149b6a696074e0d6a4b55b801
fcelli <fedecel93@gmail.com>
2020-10-20 18:43:40 +0100

    Added STXS_incZ mode.

--- hbbjet/run_modelMaker.sh
+++ hbbjet/run_modelMaker.sh
@@ -15,7 +15,7 @@ usage() {
   printf "%s\n\n" "Options:"
   printf "\t%-20s\n\t\t%s\n\n" "-t, --tag TAG" "TAG is the production name tag"
   printf "\t%-20s\n\t\t%s\n\n" "-p, --jpath JPATH" "JPATH is the json file directory path"
-  printf "\t%-20s\n\t\t%s\n\t\t%s\n\t\t%s\n\t\t%s\n\t\t%s\n\n" "-m, --mode MODE" "- MODE=CRttbar_incl: run on CRttbar inclusive" "- MODE=CRttbar_bins: run on CRttbar pT bins" "- MODE=CRttbar: run on CRttbar inclusive and pT bins" "- MODE=SR_incl: run on SR inclusive" "- MODE=all: run on everything"
+  printf "\t%-20s\n\t\t%s\n\t\t%s\n\t\t%s\n\t\t%s\n\t\t%s\n\t\t%s\n\t\t%s\n\n" "-m, --mode MODE" "- MODE=CRttbar_incl: run on CRttbar inclusive" "- MODE=CRttbar_bins: run on CRttbar pT bins" "- MODE=CRttbar: run on all CRttbar modes" "- MODE=SR_incl: run on SR inclusive" "- MODE=STXS_incZ: run on STXS incZ bins" "- MODE=SR: run on all SR modes" "- MODE=all: run on everything"
   printf "\t%-20s\n\t\t%s\n\n" "-c, --condor" "Submit jobs to HTCondor"
   printf "\t%-20s\n\t\t%s\n\n" "-h, --help" "Display this help and exit"
 }
@@ -37,7 +37,7 @@ while [ "$1" != "" ]; do
     -c | --condor )	CONDOR=true
 			;;
     -h | --help )	usage
-			exit
+			exit 0 
 			;;
     * )			usage
 			exit 1
@@ -48,7 +48,7 @@ done
 #mandatory arguments check
 if [ ! $TAG ] || [ ! $JPATH ] || [ ! $MODE ]; then
   usage
-  exit
+  exit 1
 fi
 
 #handle condor job submission
@@ -69,6 +69,7 @@ fi
 do_CRttbar_incl=false
 do_CRttbar_bins=false
 do_SR_incl=false
+do_STXS_incZ=false
 
 #run options
 case $MODE in
@@ -79,13 +80,19 @@ case $MODE in
 			;;
   CRttbar_bins )	do_CRttbar_bins=true
 			;;
+  SR )                  do_SR_incl=true
+                        do_STXS_incZ=true
+                        ;;
   SR_incl )		do_SR_incl=true
          		;;
+  STXS_incZ )		do_STXS_incZ=true
+			;;
   all )                 do_CRttbar_incl=true
 			do_CRttbar_bins=true
 			do_SR_incl=true
+			do_STXS_incZ=true
 			;;
-  * )    		printf "Error: unexpected MODE argument.\n"
+  * )    		printf "Error: unexpected MODE value.\n"
          		exit 1
 esac
 
@@ -118,6 +125,21 @@ if $do_SR_incl; then
     echo $cmd
     eval $cmd
   done
+fi
+
+#run on SR STXS incZ bins
+if $do_STXS_incZ; then
+  echo "Running on SR STXS incZ bins..."
+   for reg in 'l' 's'; do
+     for bin in '0' '1' '2'; do
+        if [ "${reg}" == "l" ] && [ "${bin}" == "0" ]; then
+          continue
+        fi
+        cmd="${condor_prefix}python modelMaker/simple_auto.py ${JPATH}STXS_incZ_AsimovSR${reg^}${bin}.json ${BINW_SR} STXS_incZ ${TAG} -c ${reg} -b ${bin}"
+        echo $cmd
+        eval $cmd
+     done
+   done 
 fi
 
 #return to base dir after condor submission
