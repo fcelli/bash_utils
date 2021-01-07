7cb1854bcc66ee522573dd78065dba08b3338838
fcelli <fedecel93@gmail.com>
2021-01-07 16:56:47 +0000

    Add fitting framework setup script

---
+++ hbbjet/setup_xmlfit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#title          : setup_xmlfit.sh
+#description    : Script for setting up the hbbjet fitting framework (xmlfit_boostedhbb). 
+#author         : fcelli
+
+setupATLAS
+
+lsetup "views LCG_97_ATLAS_1 x86_64-centos7-gcc8-opt"
+
+source setup.sh
